319d148a583058b520dd5fe2aac85783ec251596
Uneku Ogbaje <unekuogbaje@gmail.com>
2023-09-21 19:05:16 +0100

    Implemented chat engie UI using react-chat-engine and styled it using css.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatEngine
+      height="100vh"
+      projectID="501888d8-e13d-4b85-acaf-41a61c9d4991"
+      userName="Flo"
+      userSecret="qwertyuiop"
+      />
     </div>
   );
 }
