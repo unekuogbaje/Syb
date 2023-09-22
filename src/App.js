8097809cdd5f067816d87c751b7b66b53b72a90c
Uneku Ogbaje <unekuogbaje@gmail.com>
2023-09-22 22:50:38 +0100

    updated the chatfeed to render read reciepts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
+// import { ChatEngine, ChatFeed } from 'react-chat-engine';
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
 function App() {
@@ -9,6 +12,8 @@ function App() {
       projectID="501888d8-e13d-4b85-acaf-41a61c9d4991"
       userName="Flo"
       userSecret="qwertyuiop"
+      renderChatFeed = { (chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage = { () => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
       />
     </div>
   );
